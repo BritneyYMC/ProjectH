edf73b443563afafb3555ce652fbef67ca78947e
BritneyYMC <155485426+BritneyYMC@users.noreply.github.com>
2024-01-04 20:45:34 -0500

    Update DemoH.java

--- src/h/DemoH.java
+++ src/h/DemoH.java
@@ -5,5 +5,21 @@ public class DemoH {
 	public static void main(String[] args) {
 		System.out.println("Tomorrow is Friday");
 		
-	}
+	char setGender = 'F';
+		String getGender = "";
+		
+		switch(setGender) {
+		case 'M':
+		case 'm':
+			getGender = "MALE";
+			break;
+		case 'F':
+		case 'f':
+			getGender = "FEMALE";
+			break;
+		default:
+			getGender = "OTHERS";
+		}
+		System.out.println(getGender);
+}
 }
